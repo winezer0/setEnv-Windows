719e43485f21d2ffb5d3d097ebf728ee3ecf0667
WINEZERO <kissloo@foxmail.com>
2025-05-07 13:50:10 +0800

    Update setenv-add2path.bat

--- setenv-cmd/setenv-add2path.bat
+++ setenv-cmd/setenv-add2path.bat
@@ -1,9 +1,9 @@
 ::将当前路径加入path,需要管理员权限运行
 @echo off
 echo %~dp0
-setenv.exe -g "PATH"
-echo setenv.exe -a "PATH" "%~dp0"
-setenv.exe -a "PATH" "%~dp0"
+setenv.exe -g PATH
+echo setenv.exe -a PATH %~dp0
+setenv.exe -a  PATH  %~dp0
 
 set path=test
 echo %Path%
