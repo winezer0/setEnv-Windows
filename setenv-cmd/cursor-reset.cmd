8551eabe71767c7bacf714b2af0583897b0d8cee
WINEZERO <kissloo@foxmail.com>
2025-06-03 10:48:30 +0800

    Create cursor-reset.cmd

---
+++ setenv-cmd/cursor-reset.cmd
@@ -0,0 +1,1 @@
+powershell -Command "irm https://aizaozao.com/accelerate.php/https://raw.githubusercontent.com/yuaotian/go-cursor-help/refs/heads/master/scripts/run/cursor_win_id_modifier.ps1 | iex"
